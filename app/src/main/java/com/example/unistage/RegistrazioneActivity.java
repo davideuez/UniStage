6b96b25ce38bb64522662ba8ae07293c1c2b0145
Luca Calearo <luca.calearo@studenti.unitn.it>
2020-12-15 21:13:19 +0100

    Aggiunti controlli sui dati nella activity Registrazione

--- app/src/main/java/com/example/unistage/RegistrazioneActivity.java
+++ app/src/main/java/com/example/unistage/RegistrazioneActivity.java
@@ -3,47 +3,49 @@ package com.example.unistage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrazioneActivity extends AppCompatActivity {
 
     private DatabaseReference fdbr;
+    String emailText, pswText, pswConfermaText;
+
     public static int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione);
+
         final RadioButton rbprof = findViewById(R.id.profrb_signup_id);
         final RadioButton rbstud = findViewById(R.id.studenterb_signup_id);
         final RadioButton rbtuto = findViewById(R.id.tutorrb_signup_id);
         final Button reg = findViewById(R.id.registbutton_signup_id);
         final TextView accediqui = findViewById(R.id.accediqui);
 
+        final TextInputEditText email = findViewById(R.id.registrazione_email);
+        final TextInputEditText psw = findViewById(R.id.registrazione_password);
+        final TextInputEditText pswConferma = findViewById(R.id.registrazione_confermapassword);
+
 
         rbprof.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 i=1;
-                if(rbprof.isChecked()){
-                    if (rbstud.isChecked()){
-                        rbstud.toggle();
-                    }
-                    if (rbtuto.isChecked()) {
-                        rbtuto.toggle();
-                    }
-                }
+                rbstud.setChecked(false);
+                rbtuto.setChecked(false);
             }
         });
 
@@ -51,14 +53,8 @@ public class RegistrazioneActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 i=2;
-                if(rbstud.isChecked()){
-                    if (rbprof.isChecked()){
-                        rbprof.toggle();
-                    }
-                    if (rbtuto.isChecked()) {
-                        rbtuto.toggle();
-                    }
-                }
+                rbprof.setChecked(false);
+                rbtuto.setChecked(false);
             }
         });
 
@@ -66,14 +62,8 @@ public class RegistrazioneActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 i=3;
-                if(rbtuto.isChecked()){
-                    if (rbstud.isChecked()){
-                        rbstud.toggle();
-                    }
-                    if (rbprof.isChecked()) {
-                        rbprof.toggle();
-                    }
-                }
+                rbstud.setChecked(false);
+                rbprof.setChecked(false);
             }
         });
 
@@ -85,33 +75,64 @@ public class RegistrazioneActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Utente u = new Utente();
-                switch(i){
-                    case 1:
-                        u.ruolo = "Professore";
-                        fdbr.child("email_utente_x").setValue(u);
-                        Intent j = new Intent(RegistrazioneActivity.this, HomeProfessoreActivity.class);
-                        startActivity(j);
-                        break;
 
-                    case 2:
-                        u.ruolo = "Studente";
-                        fdbr.child("email_utente_x").setValue(u);
-                        Intent k = new Intent(RegistrazioneActivity.this, HomeActivity.class);
-                        startActivity(k);
-                        break;
+                pswText = psw.getText().toString();
+                emailText = email.getText().toString();
+                pswConfermaText = pswConferma.getText().toString();
 
-                    case 3:
-                        u.ruolo ="Tutor";
-                        break;
+                if(i == 1) {
 
-                    case 0:
-                        break;
+                    if (!TextUtils.isEmpty(pswText) || !TextUtils.isEmpty(emailText) || !TextUtils.isEmpty(pswConfermaText)) {
+                        if (pswText.equals(pswConfermaText)) {
+                            u.ruolo = "Professore";
+                            fdbr.child("email_utente_x").setValue(u);
+                            Intent j = new Intent(RegistrazioneActivity.this, HomeProfessoreActivity.class);
+                            startActivity(j);
+
+                        } else
+                            Toast.makeText(RegistrazioneActivity.this, "Le password non coincidono.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(RegistrazioneActivity.this, "Inserisci tutti i valori.", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+
+                if(i == 2) {
+
+                    if (!TextUtils.isEmpty(pswText) || !TextUtils.isEmpty(emailText) || !TextUtils.isEmpty(pswConfermaText)) {
+                        if (pswText.equals(pswConfermaText)) {
+                            u.ruolo = "Studente";
+                            fdbr.child("email_utente_x").setValue(u);
+                            Intent k = new Intent(RegistrazioneActivity.this, HomeActivity.class);
+                            startActivity(k);
+
+                            } else
+                                Toast.makeText(RegistrazioneActivity.this, "Le password non coincidono.", Toast.LENGTH_LONG).show();
+                        } else {
+                        Toast.makeText(RegistrazioneActivity.this, "Inserisci tutti i valori.", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+                if(i == 3) {
+
+                    if (!TextUtils.isEmpty(pswText) || !TextUtils.isEmpty(emailText) || !TextUtils.isEmpty(pswConfermaText)) {
+                        if (pswText.equals(pswConfermaText)) {
+                            u.ruolo = "Tutor";
+                        } else
+                            Toast.makeText(RegistrazioneActivity.this, "Le password non coincidono.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(RegistrazioneActivity.this, "Inserisci tutti i valori.", Toast.LENGTH_LONG).show();
+                    }
+
                 }
 
             }
         });
 
     }
+
 
     @Override
     public void onBackPressed() {
