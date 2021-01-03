a69b35c050b17d54d1d3b48f963bac7d45f01187
Luca Calearo <luca.calearo@studenti.unitn.it>
2021-01-03 23:06:56 +0100

    Creazione fragment iter tirocinio

---
+++ app/src/main/java/com/example/unistage/IterEsternoFrag.java
@@ -0,0 +1,25 @@
+package com.example.unistage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class IterEsternoFrag extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_iteresterno, container, false);
+
+        return v;
+
+
+    }
+}
--- app/src/main/java/com/example/unistage/IterFrag.java
+++ app/src/main/java/com/example/unistage/IterFrag.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class IterFrag extends Fragment {
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,11 +17,27 @@ public class IterFrag extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_itertirociniodurante, container, false);
 
+        final TextView interno = (TextView)v.findViewById(R.id.tirocinio_interno);
+        interno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getChildFragmentManager().beginTransaction().replace(R.id.container_iter, new IterInternoFrag()).commit();
+            }
+        });
+
+        final TextView esterno = (TextView)v.findViewById(R.id.tirocinio_esterno);
+        esterno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getChildFragmentManager().beginTransaction().replace(R.id.container_iter, new IterEsternoFrag()).commit();
+            }
+        });
+
         return v;
+
+
     }
 }
