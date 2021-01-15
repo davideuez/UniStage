dedb6ca791b4cc599765a62f3547954bea953d32
Luca Calearo <luca.calearo@studenti.unitn.it>
2021-01-15 15:34:23 +0100

    Aggiunta Adapter Commenti

--- app/src/main/java/com/example/unistage/TaskFrag.java
+++ app/src/main/java/com/example/unistage/TaskFrag.java
@@ -1,13 +1,29 @@
 package com.example.unistage;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import static android.widget.LinearLayout.VERTICAL;
 
 public class TaskFrag extends Fragment {
+
+    String commentoText;
+    View v = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,7 +32,38 @@ public class TaskFrag extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_tasks, container, false);
+        v = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        final EditText commento = v.findViewById(R.id.commento);
+        final ImageView commenta = v.findViewById(R.id.commenta);
+        final ArrayList<String> mComment = new ArrayList<String>();
+
+
+        commenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                commentoText = commento.getText().toString();
+
+                if(!TextUtils.isEmpty(commentoText)){
+
+                    mComment.add(commento.getText().toString());
+                    RecyclerView rv = (RecyclerView) v.findViewById(R.id.recycler_commenttask);
+
+                    final AdapterCommenti listAdapterCommenti = new AdapterCommenti(mComment);
+                    rv.setAdapter(listAdapterCommenti);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                    rv.setLayoutManager(layoutManager);
+
+                    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rv.getContext(), VERTICAL);
+                    rv.addItemDecoration(dividerItemDecoration);
+
+                }
+                else{
+                    Toast.makeText(getActivity(), "Commento vuoto", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         return v;
     }
 
