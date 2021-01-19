57faaedfad79c525aaffac35d9231c636c8a8fe2
Luca Calearo <luca.calearo@studenti.unitn.it>
2021-01-19 15:50:15 +0100

    Fixing design Scheda dettagli tirocinio, Walkthrough e Registrazione

--- app/src/main/java/com/example/unistage/Walkthrough3Activity.java
+++ app/src/main/java/com/example/unistage/Walkthrough3Activity.java
@@ -16,32 +16,20 @@ public class Walkthrough3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walkthrough3);
 
-        String userid = "billy è un cazzo di negro";
-        DatabaseReference mDatabase;
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        final Button avanti2 = findViewById(R.id.avanti2);
-        final Button salta2 = findViewById(R.id.skip2);
 
-        avanti2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Walkthrough3Activity.this, Walkthrough4Activity.class);
-                startActivity(i);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-            }
-        });
+        final Button registrati = findViewById(R.id.registrati);
 
-        salta2.setOnClickListener(new View.OnClickListener() {
+        registrati.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Walkthrough3Activity.this, RegistrazioneActivity.class);
                 startActivity(i);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         });
+
     }
 
-    @Override
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
