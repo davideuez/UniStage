b47eda55d028368e5d981ca15bb5593349fb48c7
Luca Calearo <luca.calearo@studenti.unitn.it>
2021-01-23 14:02:03 +0100

    Modifica nome-cognome-matricola in home durante

--- app/src/main/java/com/example/unistage/HomeStudenteDURANTEActivity.java
+++ app/src/main/java/com/example/unistage/HomeStudenteDURANTEActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +14,22 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeStudenteDURANTEActivity extends AppCompatActivity {
+    String nc;
+    int m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homestudente_durante);
+
+        nc = LoginActivity.u_loggato.nome + " " + LoginActivity.u_loggato.cognome;
+        final TextView nomeCognome = findViewById(R.id.nomecognome_durante);
+        nomeCognome.setText(nc);
+
+        m = LoginActivity.u_loggato.matricola;
+        final TextView matricolaDurante = findViewById(R.id.matricola_durante);
+        matricolaDurante.setText("Matricola: "+m);
+
+
         final BottomNavigationView btv = findViewById(R.id.bottombar_studentedurante);
         getSupportFragmentManager().beginTransaction().replace(R.id.card_container_durante, new TirocinioDuranteFrag()).commit();
         btv.setOnNavigationItemSelectedListener(btnm);
